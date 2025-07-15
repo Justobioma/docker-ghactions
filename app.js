035cd0c6c12be5aa5ab74137e1439a064c4e6e7e
Justobioma <justobioma@outlook.com>
2025-07-15 16:10:05 +0100

    Edited Hello word text

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello from Node.js CI/CD app!');
+  res.send('Hello from Node.js CI/CD app on GitHub!');
 });
 
 app.listen(PORT, () => {
